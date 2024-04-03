5a4592a3c28cf64a62519b929bd370d3f85109ba
vietanhtran2710 <vietanhtran.uet@gmail.com>
2024-04-03 10:14:16 +0700

    Solve Roman to Integer problem

---
+++ roman-to-int/src/main.rs
@@ -0,0 +1,43 @@
+use std::collections::HashMap;
+
+pub fn roman_to_int(s: String) -> i32 {
+    let mut result: i32 = 0;
+    let char_to_value: HashMap<char, i32> = HashMap::from([
+        ('I', 1),
+        ('V', 5),
+        ('X', 10),
+        ('L', 50),
+        ('C', 100),
+        ('D', 500),
+        ('M', 1000),
+    ]);
+    let mut previous_char: char = ' ';
+    for current_char in s.chars() {
+        result += char_to_value.get(&current_char).unwrap();
+        if previous_char != ' ' {
+            if previous_char == 'I' {
+                if current_char == 'V' || current_char == 'X' {
+                    result -= char_to_value.get(&previous_char).unwrap() * 2;
+                }
+            }
+            if previous_char == 'X' {
+                if current_char == 'L' || current_char == 'C' {
+                    result -= char_to_value.get(&previous_char).unwrap() * 2;
+                }
+            }
+            if previous_char == 'C' {
+                if current_char == 'D' || current_char == 'M' {
+                    result -= char_to_value.get(&previous_char).unwrap() * 2;
+                }
+            }
+        }
+        previous_char = current_char;
+    }
+    return result;
+}
+
+fn main() {
+    println!("{}", roman_to_int("III".to_string()));
+    println!("{}", roman_to_int("LVIII".to_string()));
+    println!("{}", roman_to_int("MCMXCIV".to_string()));
+}
