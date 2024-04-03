12105c327626881d7d01bbffaedb3d413365cfaf
Viet Anh Tran <42197149+vietanhtran2710@users.noreply.github.com>
2024-04-03 21:09:52 +0700

    Solve Remove Element problem

---
+++ remove-element/src/main.rs
@@ -0,0 +1,14 @@
+pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+    let mut index = 0;
+    while index < nums.len() {
+        while index < nums.len() && nums[index] == val {
+            nums.remove(index);
+        }
+        index += 1
+    }
+    return nums.len().try_into().unwrap();
+}
+
+fn main() {
+    println!("Hello, world!");
+}
