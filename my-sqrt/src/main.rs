076049c9bccfe1e7a60bd1e9c759ae4b282fba97
vietanhtran2710 <vietanhtran.uet@gmail.com>
2024-04-04 13:51:33 +0700

    Solve sqrt(x) problem

---
+++ my-sqrt/src/main.rs
@@ -0,0 +1,39 @@
+pub fn my_sqrt(x: i32) -> i32 {
+    if x <= 1 {
+        return x;
+    }
+    let (mut start, mut end) = (1, x / 2);
+    let mut middle = (x / 2 + 1) / 2;
+    loop {
+        if start >= end {
+            let min_val = std::cmp::min(start, end);
+            if x / min_val < min_val {
+                println!("here");
+                return middle;
+            } else {
+                return min_val;
+            }
+        }
+        middle = (start + end) / 2;
+        if x / middle == middle {
+            return middle;
+        }
+        if x / middle < middle {
+            end = middle - 1;
+        } else {
+            if x / (middle + 1) < (middle + 1) {
+                return middle;
+            }
+            start = middle + 1;
+        }
+    }
+}
+
+fn main() {
+    println!("{}", my_sqrt(4));
+    println!("{}", my_sqrt(8));
+    println!("{}", my_sqrt(1));
+    println!("{}", my_sqrt(6));
+    println!("{}", my_sqrt(2147395599));
+    println!("{}", my_sqrt(2147483647));
+}
