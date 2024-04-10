eeb2901d26af9223322b8b1fd818094b1859949a
vietanhtran2710 <vietanhtran.uet@gmail.com>
2024-04-10 13:25:27 +0700

    Solve Prime Number of Set Bits in Binary Representation problem

---
+++ count-prime-set-bits/src/main.rs
@@ -0,0 +1,20 @@
+use std::collections::HashSet;
+
+pub fn count_prime_set_bits(left: i32, right: i32) -> i32 {
+    let mut result = 0;
+    for value in left..(right + 1) {
+        let (mut num, mut count) = (value, 0);
+        while num > 0 {
+            count += if num % 2 == 1 { 1 } else { 0 };
+            num /= 2;
+        }
+        if HashSet::from([2, 3, 5, 7, 11, 13, 17, 19]).contains(&count) {
+            result += 1;
+        };
+    }
+    return result;
+}
+
+fn main() {
+    println!("{}", count_prime_set_bits(6, 10));
+}
