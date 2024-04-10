f42b1e494482610df40931dc32b6d33ac841f753
vietanhtran2710 <vietanhtran.uet@gmail.com>
2024-04-10 15:17:23 +0700

    Update solution for Running Sum problem

--- running-sum/src/main.rs
+++ running-sum/src/main.rs
@@ -1,3 +1,13 @@
+pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+    let mut result = vec![];
+    let mut r_sum = 0;
+    for item in nums {
+        result.push(r_sum + item);
+        r_sum += item;
+    }
+    return result;
+}
+
 fn main() {
     println!("Hello, world!");
 }
