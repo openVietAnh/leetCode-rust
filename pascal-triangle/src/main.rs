c6405ff265045417e26cebe9528225a556b779e3
Viet Anh Tran <42197149+vietanhtran2710@users.noreply.github.com>
2024-04-14 20:48:28 +0700

    Solve Pascal Triangle Problem

---
+++ pascal-triangle/src/main.rs
@@ -0,0 +1,23 @@
+pub fn generate(num_rows: i32) -> Vec<Vec<i32>> {
+    if num_rows == 1 {
+        vec![vec![1]]
+    } else if num_rows == 2 {
+        vec![vec![1], vec![1, 1]]
+    } else {
+        let mut result = vec![vec![1], vec![1, 1]];
+        for i in 2..num_rows {
+            let mut new_row: Vec<i32> = vec![1];
+            let last_row = result.last().unwrap();
+            for j in 0..last_row.len() - 1 {
+                new_row.push(last_row[j] + last_row[j + 1]);
+            }
+            new_row.push(1);
+            result.push(new_row.clone());
+        }
+        result
+    }
+}
+
+fn main() {
+    println!("{:?}", generate(5));
+}
