fc726f5f2cc7f2ba92da6d0a19cd209618f9d64f
vietanhtran2710 <vietanhtran.uet@gmail.com>
2024-04-15 18:05:00 +0700

    Add missed solution code

--- add-digits/src/main.rs
+++ add-digits/src/main.rs
@@ -1,3 +1,14 @@
+pub fn add_digits(num: i32) -> i32 {
+    if num <= 9 {
+        return num;
+    }
+    if num % 9 == 0 {
+        num - ((num / 9 - 1) * 9)
+    } else {
+        num - ((num / 9) * 9)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", add_digits(38));
 }
