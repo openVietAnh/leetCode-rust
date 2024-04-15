fa94bbd88b96d8c92a7c0280f80b18df686da7d3
vietanhtran2710 <vietanhtran.uet@gmail.com>
2024-04-15 16:54:39 +0700

    Solve Is Anagram problem

---
+++ is-anagram/src/main.rs
@@ -0,0 +1,38 @@
+use std::collections::HashMap;
+
+pub fn is_anagram(s: String, t: String) -> bool {
+    let mut counter1 = HashMap::new();
+    let mut counter2 = HashMap::new();
+    for c in s.chars() {
+        counter1
+            .entry(c)
+            .and_modify(|value| *value += 1)
+            .or_insert(1);
+    }
+    for c in t.chars() {
+        counter2
+            .entry(c)
+            .and_modify(|value| *value += 1)
+            .or_insert(1);
+    }
+    if counter1.keys().len() != counter2.keys().len() {
+        return false;
+    }
+    for (key, value) in counter1.iter() {
+        if !counter2.contains_key(key) {
+            return false;
+        }
+        if counter2.get(key).unwrap() != value {
+            return false;
+        }
+    }
+    true
+}
+
+fn main() {
+    println!(
+        "{}",
+        is_anagram(String::from("anagram"), String::from("nagaram"))
+    );
+    println!("{}", is_anagram(String::from("rat"), String::from("car")));
+}
